aa223c4143431e30e2d86a01bd84f1dc18752a88
Gabriel <glake@purdue.edu>
2017-04-08 20:17:41 +0000

    latest edits

--- top_block.py
+++ top_block.py
@@ -2,7 +2,7 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: Top Block
-# Generated: Sat Apr  1 22:10:36 2017
+# Generated: Sat Apr  8 19:17:17 2017
 ##################################################
 
 from gnuradio import blocks
@@ -10,27 +10,26 @@ from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
-from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
 import osmosdr
-import wx
+import time
 
-class top_block():
+class top_block(gr.top_block):
 
     def __init__(self):
-        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+        gr.top_block.__init__(self, "Top Block")
 
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 2e6
+        self.samp_rate = samp_rate = 3200000
 
         ##################################################
         # Blocks
         ##################################################
         self.rtlsdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + "" )
         self.rtlsdr_source_0.set_sample_rate(samp_rate)
-        self.rtlsdr_source_0.set_center_freq(93.5, 0)
+        self.rtlsdr_source_0.set_center_freq(102.895e6, 0)
         self.rtlsdr_source_0.set_freq_corr(0, 0)
         self.rtlsdr_source_0.set_dc_offset_mode(0, 0)
         self.rtlsdr_source_0.set_iq_balance_mode(0, 0)
@@ -39,10 +38,11 @@ class top_block():
         self.rtlsdr_source_0.set_if_gain(20, 0)
         self.rtlsdr_source_0.set_bb_gain(20, 0)
         self.rtlsdr_source_0.set_antenna("", 0)
-        self.rtlsdr_source_0.set_bandwidth(1e6, 0)
-          
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, "/home/pi/gnuradio_blocks/testRecord.dat", False)
-        self.blocks_file_sink_0.set_unbuffered(False)
+        self.rtlsdr_source_0.set_bandwidth(0, 0)
+        
+	datFile = open("testRecord.dat","wb+")
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, "../AAE490/testRecord.dat", False)
+        self.blocks_file_sink_0.set_unbuffered(True)
 
         ##################################################
         # Connections
@@ -59,18 +59,11 @@ class top_block():
         self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
 
 if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print "Warning: failed to XInitThreads()"
     parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
     (options, args) = parser.parse_args()
     if gr.enable_realtime_scheduling() != gr.RT_OK:
         print "Error: failed to enable realtime scheduling."
     tb = top_block()
-    tb.Start(True)
-    tb.Wait()
+    tb.start()
+    tb.wait()	
+    tb.stop()
