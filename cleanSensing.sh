9f91fce43684908b01952d05f7817e19952a726f
Gabriel <glake@purdue.edu>
2017-04-11 04:20:58 +0000

    4/11/2017

--- cleanSensing.sh
+++ cleanSensing.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 
-sudo rm /etc/*.dat
-sudo rm /etc/*.dat.hdr
+sudo rm /home/DBT490/etc/testRecord.dat
+sudo rm /home/DBT490/etc/testRecord.dat.hdr
 sudo rm ~/startup.log
 echo "dat files and startup.log removed"
 
